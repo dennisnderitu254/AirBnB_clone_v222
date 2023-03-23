8391597392f57a75fc67053f3c03b458ff9a4ff1
dennisnderitu254 <dknderitu@gmail.com>
2023-03-23 12:16:00 +0300

    debugging models

--- models/place.py
+++ models/place.py
@@ -49,18 +49,3 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
-
-# class Place(BaseModel):
-#     """place class"""
-#     city_id = ""
-#     user_id = ""
-#     name = ""
-#     description = ""
-#     number_rooms = 0
-#     number_bathrooms = 0
-#     max_guest = 0
-#     price_by_night = 0
-#     latitude = 0.0
-#     longitude = 0.0
-#     amenity_ids = ""
-#     place_amenity = ""
