0b255e408578215666e9f9e466223ab2422a6e7a
Flesier Akinyi <flesieronyango18@gmail.com>
2023-03-23 11:50:06 +0300

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,71 +1,43 @@
 #!/usr/bin/python3
-"""Tests city"""
-import unittest
-import pep8
-import os
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.user import User
-from models.base_model import BaseModel
+import os
 
 
-class TestCity(unittest.TestCase):
-    """unittests for basemodel"""
+class test_User(test_basemodel):
+    """ test class for user model"""
 
-    @classmethod
-    def setUp(cls):
-        """creates class"""
-        cls.testUser = User()
-        cls.testUser.email = "email"
-        cls.testUser.password = "xxx"
-        cls.testUser.first_name = "first"
-        cls.testUser.last_name = "last"
+    def __init__(self, *args, **kwargs):
+        """ user test class init"""
+        super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User
 
-    @classmethod
-    def tearDown(cls):
-        """deletes test class"""
-        del cls.testUser
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
+    def test_first_name(self):
+        """ testing user first anme attr"""
+        new = self.value()
+        self.assertEqual(type(new.first_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
-    def test_pep8(self):
-        """tests pep8"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/user.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_last_name(self):
+        """ testing user last name attr"""
+        new = self.value()
+        self.assertEqual(type(new.last_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
-    def test_docstrings(self):
-        """tests docstrings"""
-        self.assertTrue(len(User.__doc__) > 0)
-        for func in dir(User):
-            self.assertTrue(len(func.__doc__) > 0)
+    def test_email(self):
+        """ testing user email attr"""
+        new = self.value()
+        self.assertEqual(type(new.email), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
-    def test_init_and_class_variables(self):
-        """tests init and class variables"""
-        self.assertTrue(isinstance(self.testUser, User))
-        self.assertTrue(issubclass(type(self.testUser), BaseModel))
-        self.assertTrue('email' in self.testUser.__dict__)
-        self.assertTrue('id' in self.testUser.__dict__)
-        self.assertTrue('created_at' in self.testUser.__dict__)
-        self.assertTrue('updated_at' in self.testUser.__dict__)
-        self.assertTrue('password' in self.testUser.__dict__)
-        self.assertTrue('first_name' in self.testUser.__dict__)
-        self.assertTrue('last_name' in self.testUser.__dict__)
-
-    def test_save(self):
-        self.testUser.save()
-        self.assertTrue(self.testUser.updated_at != self.testUser.created_at)
-
-    def test_strings(self):
-        self.assertEqual(type(self.testUser.email), str)
-        self.assertEqual(type(self.testUser.password), str)
-        self.assertEqual(type(self.testUser.first_name), str)
-        self.assertEqual(type(self.testUser.first_name), str)
-
-    def test_to_dict(self):
-        self.assertEqual('to_dict' in dir(self.testUser), True)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
+    def test_password(self):
+        """ testing user password attr"""
+        new = self.value()
+        self.assertEqual(type(new.password), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
