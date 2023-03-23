ca88b05f10978ac9cc4eec5315360fef10fc1f44
Flesier Akinyi <flesieronyango18@gmail.com>
2023-03-23 11:48:23 +0300

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,75 +1,36 @@
 #!/usr/bin/python3
-"""tests for class review"""
-import unittest
-import os
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
-from models.base_model import BaseModel
-import pep8
+import os
 
 
-class TestReview(unittest.TestCase):
-    """test for review class"""
+class test_review(test_basemodel):
+    """ review test class"""
 
-    @classmethod
-    def setUpClass(cls):
-        """set up before functions"""
-        cls.newreview = Review()
-        cls.newreview.place_id = "7272728"
-        cls.newreview.user_id = "Lul"
-        cls.newreview.text = "Place is meh"
+    def __init__(self, *args, **kwargs):
+        """ review class init"""
+        super().__init__(*args, **kwargs)
+        self.name = "Review"
+        self.value = Review
 
-    @classmethod
-    def teardown(cls):
-        """teardown"""
-        del cls.newreview
+    def test_place_id(self):
+        """ testing review place_id attr"""
+        new = self.value()
+        self.assertEqual(type(new.place_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
-    def tearDown(self):
-        """teardown"""
-        try:
-            os.remove("file.json")
-        except:
-            pass
+    def test_user_id(self):
+        """ testing review user_id attr"""
+        new = self.value()
+        self.assertEqual(type(new.user_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
-    def test_pep8_review(self):
-        """tests pep8"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/review.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-    def test_docstring_review(self):
-        """test for docstring"""
-        self.assertIsNotNone(Review.__doc__)
-
-    def test_attributes_review(self):
-        """checking instance attributes"""
-        self.assertTrue('id' in self.newreview.__dict__)
-        self.assertTrue('created_at' in self.newreview.__dict__)
-        self.assertTrue('updated_at' in self.newreview.__dict__)
-        self.assertTrue('place_id' in self.newreview.__dict__)
-        self.assertTrue('text' in self.newreview.__dict__)
-        self.assertTrue('user_id' in self.newreview.__dict__)
-
-    def test_subclass_review(self):
-        """test for inheritance"""
-        self.assertTrue(issubclass(self.newreview.__class__, BaseModel), True)
-
-    def test_attr_types_review(self):
-        """test attribute type in instance"""
-        self.assertEqual(type(self.newreview.text), str)
-        self.assertEqual(type(self.newreview.place_id), str)
-        self.assertEqual(type(self.newreview.user_id), str)
-
-    def test_save(self):
-        """test save function"""
-        self.newreview.save()
-        self.assertNotEqual(self.newreview.created_at,
-                            self.newreview.updated_at)
-
-    def test_to_dict(self):
-        """test dictionary is there"""
-        self.assertEqual('to_dict' in dir(self.newreview), True)
-
-
-if __name__ == "__main__":
-    unittest.main()
-
+    def test_text(self):
+        """ testing review text attr"""
+        new = self.value()
+        self.assertEqual(type(new.text), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
